651e7228a694b8fb203c0570ba41140bfb1d0efa
sreyesg <santiagoreyes.g@gmail.com>
2024-09-10 16:55:01 -0700

    add new route and new.ejs form

--- server.js
+++ server.js
@@ -2,21 +2,24 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-require('./models/fruit.js')
 
-//database connection
+//DATABASE CONNECTION
 mongoose.connect(process.env.MONGODB_URI)
 // log connection status to terminal
 mongoose.connection.on("connected", ()=> {
-    console.log(`connected to MongoDB ${mongoose.name}.`)
+    console.log(`connected to MongoDB ${mongoose.connection.name}.`)
     // console.log(mongoose.connection)
 })
+
+    // require model
+const Fruit = require('./models/fruit.js')
 
 //landing page route
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
+// create new route: it will send a form to user
 
 
 
