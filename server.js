95f5e19fc7894c9b219bf1dac6d650711a6b5498
sreyesg <santiagoreyes.g@gmail.com>
2024-09-10 15:56:12 -0700

    require dotenv, mongoose and add database connection

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-// require('dotenv').config()
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+//database connection
+mongoose.connect(process.env.MONGODB_URI)
+// log connection status to terminal
+mongoose.connection.on("connected", ()=> {
+    console.log(`connected to MongoDB ${mongoose.connection.name}.`)
+})
 
 //landing page route
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index.ejs')
 })
 
 
