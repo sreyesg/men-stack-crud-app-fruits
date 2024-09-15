736f27ecf2c35bdd40a3b3e9d1ffffce81b1ef5f
sreyesg <santiagoreyes.g@gmail.com>
2024-09-15 16:22:50 -0700

    second review on createUser

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,17 +6,18 @@ const signUpForm = (req, res) => {
 }
 
 const createUser = async(req, res)=>{
-    const userInDatabase = await User.find({username: req.body.username})
+    const userInDatabase = await User.findOne({username: req.body.username})
     if(userInDatabase){
         return res.send('the username is already taken, please use a different username')
     
-    } if(req.body.password !== req.body.confirmPassword){
+    } 
+    if(req.body.password !== req.body.confirmPassword){
         return res.send('Password did not match, please try it again')
     } 
-    passwordHashed = bcrypt.hashSync(req.body.password, 10)
+    const passwordHashed = bcrypt.hashSync(req.body.password, 10)
     req.body.password = passwordHashed
     await User.create(req.body)
-    res.redirect('/auth/sign-up')
+    res.send('form Submission accepted')
 }
 
 
--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 // Schema
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
+    username: {type: String, required: true},
     password: {type: String, require: true},
 })
 
